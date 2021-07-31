35c80f8fed408117d69fc1bcdfb8565a4078ea08
Boris Sulicenko <b.sulicenko@gmail.com>
2021-07-31 15:59:52 +0200

    #17

--- product-service/src/product/product.controller.ts
+++ product-service/src/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { DeepPartial } from 'typeorm';
 import { Product } from './product.entity';
 import { ProductService } from './product.service';
@@ -10,5 +10,15 @@ export class ProductController {
   @Post()
   async create(@Body() payload: DeepPartial<Product>) {
     return this.productService.create(payload)
+  }
+
+  @Get()
+  async getAll() {
+    return this.productService.findAll()
+  }
+
+  @Get(':id')
+  async getByid(@Param('id') id: number) {
+    return this.productService.findById(id)
   }
 }
